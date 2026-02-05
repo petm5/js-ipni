ec7829ed263f66d40ffcb87286da0e1b5a6ae9cd
Peter Marshall <petms@proton.me>
2026-02-05 18:41:56 -0500

    export chunk threshold

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Protocol, Provider } from './provider.js'
 export { Advertisement } from './advertisement.js'
-export { EntryChunk } from './entry-chunk.js'
+export { EntryChunk, CHUNK_THRESHOLD } from './entry-chunk.js'
